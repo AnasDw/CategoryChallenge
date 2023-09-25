f3a3c2644b719e4378c4502391ef3a0eb1c5fa88
AnasDw <anas.e.dweik@gmail.com>
2023-09-25 11:28:59 +0300

    I Deleted input box

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,9 +5,9 @@ import WelcomeComponent from "../components";
 
 const HomePage = () => {
   return (
-    <div className="flex Home-style flex-row full-width-height">
-        <WelcomeComponent/>
-    </div>
+    <>
+      <div className="flex Home-style flex-row full-width-height"> </div>
+    </>
   );
 };
 
